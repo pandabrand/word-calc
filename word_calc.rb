c9292099047174cb4a2de311f0f85b25a11ab037
pandabrand <pandabrand@gmail.com>
2015-09-29 11:37:16 -0500

    removed test helper. separated the UI from the logic. logic now fully testable

--- word_calc.rb
+++ word_calc.rb
@@ -1,33 +1,49 @@
-class WordCalc
+class WordCalcUI
   attr_accessor :input, :output
-  @@commands = {"plus" => "+", "minus" => "-", "times" => "*", "divide" => "/"}
-
+  @word
   def initialize
     self.input  = $stdin
     self.output = $stdout
+    @word = WordCalc.new
+  end
+  
+  def start
+    output.puts @word.welcome
+    output.puts @word.start
+    entry = input.gets.chomp
+    send entry
   end
 
+  def send(entry)
+    result = @word.validate entry
+    output.puts result
+    unless result == 'good bye!'
+      output.puts @word.start
+      send input.gets.chomp
+    end
+  end
+end
+
+class WordCalc
+  @@commands = {"plus" => "+", "minus" => "-", "times" => "*", "divide" => "/"}
+
   def welcome
-    self.output.puts "Please use numbers '{1,2,3..}.\n'plus','minus','times', or 'divide' are the operators.\n'help' for instructions.\n'quit' to exit."
+     "Please use numbers '{1,2,3..}.\n'plus','minus','times', or 'divide' are the operators.\n'help' for instructions.\n'quit' to exit."
   end
 
   def start
-    output.puts "What would you like to calculate?"
-    entry = input.gets.chomp
-    validate(entry)
+    "What would you like to calculate?"
   end
 
   def validate(entry)
     if @@commands.keys.any?{ |key| entry.include?(key) } && entry =~ /\d/
       calculate(entry)
     elsif entry == 'help'
-      output.puts "use either \n'plus' for addition\n'minus' for substraction\n'times' for multiplication or\n'divide' for division'\nuse numbers to surround the operator, for example '4 times 8'\ntype 'quit' to exit program'"
-      start
+      "use either \n'plus' for addition\n'minus' for substraction\n'times' for multiplication or\n'divide' for division'\nuse numbers to surround the operator, for example '4 times 8'\ntype 'quit' to exit program'"
     elsif entry == 'quit'
-      output.puts "good bye!"
+      "good bye!"
     else
-      output.puts "#{entry} is not a valid operator, please type 'help' for instructions."
-      start
+      "#{entry} is not a valid operator, please type 'help' for instructions."
     end
   end
 
@@ -36,10 +52,9 @@ class WordCalc
     command_key = command_arr[1]
     command_arr.delete_at(1)
     command_arr.map! {|x| x.to_i}
-    output.puts command_arr[0].send(@@commands[command_key],command_arr[1])
-    start
+    command_arr[0].send(@@commands[command_key],command_arr[1])
   end
 end
 
-#calc = WordCalc.new
-#calc.start
+calc = WordCalcUI.new
+calc.start
--- word_calc_test.rb
+++ word_calc_test.rb
@@ -1,29 +1,38 @@
 require 'minitest/autorun'
-require './test_helper'
 require './word_calc'
 
 class WordCalcTest < MiniTest::Test
-  include TestHelper
 
   def setup
     @word = WordCalc.new
   end
 
   def test_start_message
-    input = StringIO.new
-    output = StringIO.new
-    @word.input = input
-    @word.output = output
-    @word.welcome
-    assert_equal "Please use numbers '{1,2,3..}.\n'plus','minus','times', or 'divide' are the operators.\n'help' for instructions.\n'quit' to exit.\n", output.string
+    output = @word.welcome
+    assert_equal "Please use numbers '{1,2,3..}.\n'plus','minus','times', or 'divide' are the operators.\n'help' for instructions.\n'quit' to exit.", output
   end
 
   def test_error_message
-    input = StringIO.new("five and 6\n")
-    output = StringIO.new
-    @word.input = input
-    @word.output = output
-    @word.start
-    assert_equal "Check help for instructions", output.string
+    input = "five and 6"
+    output = @word.validate(input)
+    assert_equal "five and 6 is not a valid operator, please type 'help' for instructions.", output
+  end
+
+  def test_help_message
+    input = "help"
+    output = @word.validate(input)
+    assert_equal "use either \n'plus' for addition\n'minus' for substraction\n'times' for multiplication or\n'divide' for division'\nuse numbers to surround the operator, for example '4 times 8'\ntype 'quit' to exit program'", output
+  end
+
+  def test_quit_message
+    input = "quit"
+    output = @word.validate(input)
+    assert_equal "good bye!", output
+  end
+ 
+  def test_answer_message
+    input = "4 plus 5"
+    output = @word.validate(input)
+    assert_equal 9, output
   end
 end
